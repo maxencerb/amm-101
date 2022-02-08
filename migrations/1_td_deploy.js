40204da9c8b886d4af6ccd38b59b67c2a4854d50
Maxence Raballand <47566657+maxencerb@users.noreply.github.com>
2022-02-08 15:49:34 +0100

    Did some exercices

--- migrations/1_td_deploy.js
+++ migrations/1_td_deploy.js
@@ -4,7 +4,10 @@ const Str = require('@supercharge/strings')
 var TDErc20 = artifacts.require("ERC20TD.sol");
 var ERC20 = artifacts.require("DummyToken.sol"); 
 var evaluator = artifacts.require("Evaluator.sol");
+var myERC20 = artifacts.require("myERC20.sol");
+var exerciceSolution = artifacts.require("ExerciceSolution.sol");
 
+const account = "0x3Ab484E75884b42AD86BE388D04b7B3208a5c6cD"
 
 module.exports = (deployer, network, accounts) => {
     deployer.then(async () => {
@@ -12,6 +15,7 @@ module.exports = (deployer, network, accounts) => {
         await deployEvaluator(deployer, network, accounts); 
         await setPermissionsAndRandomValues(deployer, network, accounts); 
         await deployRecap(deployer, network, accounts); 
+		await makeExercice(deployer, network, accounts);
     });
 };
 
@@ -49,6 +53,67 @@ async function deployRecap(deployer, network, accounts) {
 	console.log("TDToken " + TDToken.address)
 	console.log("dummyToken " + dummyToken.address)
 	console.log("Evaluator " + Evaluator.address)
+} 
+
+async function deployMyERC20(deployer, network, accounts, ticker, supply) {
+	MyERC20 = await myERC20.new(ticker, ticker, supply)
+	console.log("My ERC20 " + MyERC20.address)
 }
 
+async function deploySolution(deployer, network, accounts) {
+	ExerciceSolution = await exerciceSolution.new(TDToken.address, MyERC20.address, wethAddress)
+	console.log("Exercice Solution " + ExerciceSolution.address)
+}
 
+async function makeExercice(deployer, network, accounts) {
+	
+	const initialSupply = await TDToken.balanceOf(account)
+	console.log("initialSupply " + initialSupply)
+
+	// console.log("========== Exercice 1 ==========")
+	// await Evaluator.ex1_showIHaveTokens({from: account})
+	// const balance_ex1 = await TDToken.balanceOf(account)
+	// console.log("balance_ex1 " + balance_ex1)
+
+	// console.log("========== Exercice 2 ==========")
+	// await Evaluator.ex2_showIProvidedLiquidity({from: account})
+	// const balance_ex2 = await TDToken.balanceOf(account)
+	// console.log("balance_ex2 " + balance_ex2)
+
+	console.log("========== Exercice 6a ==========")
+	await Evaluator.ex6a_getTickerAndSupply({from: account})
+	const ticker = await Evaluator.readTicker(account)
+	const supply = await Evaluator.readSupply(account)
+	console.log("ticker " + ticker)
+	console.log("supply " + supply)
+	const balance_ex6a = await TDToken.balanceOf(account)
+	console.log("balance_ex6a " + balance_ex6a)
+
+	console.log("========== Exercice 6b ==========")
+	await deployMyERC20(deployer, network, accounts, ticker, supply)
+	await Evaluator.submitErc20(MyERC20.address, {from: account})
+	await Evaluator.ex6b_testErc20TickerAndSupply({from: account})
+	const balance_ex6b = await TDToken.balanceOf(account)
+	console.log("balance_ex6b " + balance_ex6b)
+
+	console.log("========== Exercice 7 ==========")
+	await deploySolution(deployer, network, accounts)
+	const erc20WETHPair = await ExerciceSolution.createPairWithERC20(wethAddress, {from: account})
+	await Evaluator.ex7_tokenIsTradableOnUniswap({from: account})
+	const balance_ex7 = await TDToken.balanceOf(account)
+	console.log("balance_ex7 " + balance_ex7)
+
+	console.log("========== Exercice 8 ==========")
+	await Evaluator.submitExercice(ExerciceSolution.address, {from: account})
+	
+
+
+
+
+
+
+	await ExerciceSolution.createPairWithERC20(dummyToken.address, {from: account})
+
+
+
+}
